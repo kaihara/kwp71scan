28f685595f6a7af8a4bf7887595180b815c74cbc
Takahiro Kaihara <takahiro.kaihara@gmail.com>
2020-03-30 21:58:16 +0900

    Update scan.ino
    Add Engine speed (but no test..)

--- scan/scan.ino
+++ scan/scan.ino
@@ -45,6 +45,7 @@ const byte ACK[] = { 1, 0x09};
 const byte ADC_BATTERY[] = { 2, 0x08, 0x01};      // ( data[3] * 0.0681 + 0.0019 , 1)
 const byte ADC_WATER_TEMP[] = { 2, 0x08, 0x03};   // ( (-0.000014482 * pow(data[3], 3) + 0.006319247 * pow(data[3], 2) - 1.35140625 * data[3] + 144.4095455), 1)
 const byte BATTERY[] = { 4, 0x01, 0x01, 0x00, 0x36}; // ( data[2] * 0.0681 + 0.0019 , 1)
+const byte ENGIN_SPEED[] = { 4, 0x01, 0x01, 0x00, 0x3a}; // ( data[2] * data[3] + 40 , 0)
 
 /* LCD Setting */
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -111,24 +112,24 @@ void loop() {
       lcd.print( (-0.000014482 * pow(data[3], 3) + 0.006319247 * pow(data[3], 2) - 1.35140625 * data[3] + 144.4095455), 1);
     }
     delay(20);
+//    lcd.setCursor(0, 1);
+//    lcd.print("DTC: 0  NO ERROR");
+
     lcd.setCursor(0, 1);
-    lcd.print("DTC: 0  NO ERROR");
-/*    
-    delay(1000);
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-    delay(1000);
-    lcd.setCursor(0, 1);
-    lcd.print("DTC: 0  NO ERROR");
-    delay(1000);
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-    delay(1000);
-    lcd.setCursor(0, 1);
-    lcd.print("DTC: 0  NO ERROR");
-    delay(1000);
-*/
-    initialized = false;
+    if ( rcv_info(ENGIN_SPEED) == false ) {
+      initialized = false;
+      lcd.print("ERROR");
+    } else {
+      lcd.print("PPM ");
+      lcd.print( data[2] * data[3] * 40);
+    }
+    delay(20);
+
+    lcd.setCursor(8, 1);
+    lcd.print("DTC: 0");
+    delay(20);
+
+    initialized = true;
     clear_lcd = false;
   }
 
